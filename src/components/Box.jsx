bfd53d65abc254e43eb83187ef63c73dec077ee8
omid <imguromid@gmail.com>
2024-08-20 16:06:40 +0430

    nodified box component

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,15 +1,10 @@
-export default function Box({ children, color }) {
-  let textColor;
-  if (color === 'pink') textColor = 'white';
-  if (color === 'blue') textColor = 'white';
-  if (color === 'white') textColor = 'pink';
-  if (color === 'yellow') textColor = 'blue';
-  if (color === 'gray') textColor = 'yellow';
+import './../App.css';
+
+export default function Box({ className, children }) {
+  const tailwindClass = `rounded-3xl my-1 py-4 px-5 relative ${className}`;
 
   return (
-    <div
-      className={`rounded-3xl py-4 px-5 bg-${color} text-${textColor} relative`}
-    >
+    <div className={tailwindClass}>
       <button className='absolute top-2 right-2 size-10 rounded-full text-3xl bg-black pb-10 text-white'>
         +
       </button>
--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -1,9 +1,9 @@
 import Box from './Box';
 
-export default function Section1() {
+export default function Section1(){
   return (
     <section>
-      <Box color='gray'>
+      <Box className='bg-gray text-yellow'>
         <span>Creating the Future</span>
         <h2>Innovative AI Developer Team</h2>
         <p>
@@ -13,17 +13,17 @@ export default function Section1() {
         </p>
       </Box>
 
-      <Box color='pink'>
+      <Box className='bg-pink text-white'>
         <h2>Transform </h2>
         <p>Rethinking business models</p>
       </Box>
 
-      <Box color='blue'>
+      <Box className='bg-blue text-white'>
         <h2>Automate</h2>
         <p>Deploying robotics and other automation tools</p>
       </Box>
 
-      <Box color='white'>
+      <Box className='bg-white text-pink'>
         <h2>Innovate</h2>
         <p>
           Achieved through a variety of methods, including experimentation,
