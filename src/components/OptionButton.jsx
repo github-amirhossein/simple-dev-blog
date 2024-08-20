f2ae35f44511b691d0928dae3b77e4f04d189dca
omid <imguromid@gmail.com>
2024-08-20 23:25:35 +0430

    create option button component & update section 2

---
+++ src/components/OptionButton.jsx
@@ -0,0 +1,15 @@
+import star from './../assets/images/star.svg';
+import logout from './../assets/images/logout.svg';
+
+export default function OptionButton({ children, icon='star' }) {
+  let iconSrc;
+  if (icon === 'star') iconSrc = star
+  if (icon === 'logout' ) iconSrc = logout
+
+  return (
+    <button className='flex items-center border px-4 py-2 rounded-full'>
+      {children}
+      <img className='pl-3' src={iconSrc} alt='' />
+    </button>
+  );
+}
--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -1,3 +1,23 @@
-export default function Section2(){
-  return <section>section 2</section>
+import OptionButton from './OptionButton';
+
+export default function Section2() {
+  return (
+    <section>
+      <h3>We offer a range of innovative solutions </h3>
+      <p>
+        Our solutions can help our clients to automate their operations,
+        optimize their workflows, and transform their businesses.
+      </p>
+      <hr />
+
+      <div className='flex flex-wrap justify-around'>
+        <OptionButton>AI development</OptionButton>
+        <OptionButton>Deep learning</OptionButton>
+        <OptionButton>Industry 4.0</OptionButton>
+        <OptionButton>Big data</OptionButton>
+        <OptionButton>Neural networks</OptionButton>
+        <OptionButton>Predictive analytics</OptionButton>
+      </div>
+    </section>
+  );
 }
