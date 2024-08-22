140d684eb17f82f2c76b4f857a121c9357f0a4e8
Amirhossein <amyrhsynshakry4@gmail.com>
2024-08-22 01:45:26 -0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,10 @@
-import Navbar from './Navbar';
+
 
 export default function Footer() {
   return (
     <footer>
-      <Navbar />
-      <p>AI Developer Team</p>
-      <p>Privacy policy</p>
+    
+     
     </footer>
   );
 }
