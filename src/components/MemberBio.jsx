49280a93658b0e16dcc0ea8d809c31ede3f910b4
omid <imguromid@gmail.com>
2024-08-22 16:57:31 +0430

    update section 2

--- src/components/MemberBio.jsx
+++ src/components/MemberBio.jsx
@@ -1,23 +1,30 @@
-import coffee from './../assets/images/coffee.svg'
+import coffee from './../assets/images/coffee.svg';
 
-export function MemberBio({ imgSrc, memberName= 'Omid' }) {
+export function MemberBio({ imgSrc, onOpenMoreInfo, memberName = 'Omid' }) {
   return (
-    <div className='grid grid-cols-3 border-2  rounded-3xl over p-2 text-sm'>
+    <div
+      onClick={onOpenMoreInfo}
+      className='grid grid-cols-3 border-2 rounded-3xl over p-2 text-sm'
+    >
       <div className='col-span-1 flex items-center rounded-full overflow-hidden'>
         <img className='h-full' src={imgSrc} alt={`${memberName} image`} />
       </div>
 
       <div className='col-span-2 pl-4 flex flex-col justify-between'>
-        <h3 className="border-l-2 border-yellow bg-gray p-1 rounded-se-2xl">{memberName}</h3>
-        <p className="text-justify">
+        <h3 className='border-l-2 border-yellow bg-gray p-1 rounded-se-2xl'>
+          {memberName}
+        </h3>
+        <p className='text-justify'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
           quisquam vel!
         </p>
 
-        <div className="flex justify-between h-8">
-          <button className="bg-yellow text-blue px-5 rounded-lg">more info</button>
+        <div className='flex justify-between h-8'>
+          <button className='bg-yellow text-blue px-5 rounded-lg'>
+            more info
+          </button>
           <button className='size-8 self-center bg-white overflow-hidden rounded-lg'>
-            <img className='scale-150' src={coffee} alt="buy me a coffe" />
+            <img className='scale-150' src={coffee} alt='buy me a coffe' />
           </button>
         </div>
       </div>
--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -1,13 +1,52 @@
+import { useState } from 'react';
 import noImage from './../assets/images/no-image-available.jpg';
 import { MemberBio } from './MemberBio';
 
 export default function Section2() {
+  const [currentBio, setCurrentBio] = useState(1);
+
+  function handleOpenMoreInfo() {
+    setCurrentBio(1);
+  }
+
+  function handleCloseMoreInfo() {
+    setCurrentBio(null);
+  }
+
   return (
-    <section className='grid grid-rows-subgrid grid-cols-1'>
-      <MemberBio imgSrc={noImage} memberName='Amir' />
-      <MemberBio imgSrc={noImage} memberName='Hooman' />
-      <MemberBio imgSrc={noImage} memberName='Hossein' />
-      <MemberBio imgSrc={noImage} memberName='Omid'/>
+    <section className='items-between relative'>
+      <h2 className='text-xl text-center pb-1'>Code Club Members Biograghie</h2>
+      <div className='grid grid-rows-subgrid gap-1 grid-cols-1'>
+        <MemberBio
+          onOpenMorInfo={handleOpenMoreInfo}
+          imgSrc={noImage}
+          memberName='Amir'
+        />
+
+        <MemberBio
+          onOpenMorInfo={handleOpenMoreInfo}
+          imgSrc={noImage}
+          memberName='Hooman'
+        />
+
+        <MemberBio
+          onOpenMorInfo={handleOpenMoreInfo}
+          imgSrc={noImage}
+          memberName='Hossein'
+        />
+
+        <MemberBio
+          onOpenMorInfo={handleOpenMoreInfo}
+          imgSrc={noImage}
+          memberName='Omid'
+        />
+      </div>
+
+      {currentBio && (
+        <div className='absolute w-full h-5/6 border-2 border-yellow bg-black top-16 z-40 left-0'>
+          <button onClick={handleCloseMoreInfo}>x</button>
+        </div>
+      )}
     </section>
   );
 }
