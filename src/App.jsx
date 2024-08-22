f48cf6a27ac289f85e5140381e51872f73ea45b0
Amirhossein <amyrhsynshakry4@gmail.com>
2024-08-22 01:45:38 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
 import './App.css';
-import Header from './components/Header';
-import Section1 from './components/Section1';
-import Section2 from './components/Section2';
-import Section3 from './components/Section3';
-import Section4 from './components/Section4';
-import Section5 from './components/Section5';
-import Section6 from './components/Section6';
 import Footer from './components/Footer';
+import { BrowserRouter  , Route , Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import About from './components/about';
+
+
+
 
 function App() {
   return (
     <>
-      <Header />
-      <main>
-        <Section1 />
-        <Section2 />
-        <Section3 />
-        <Section4 />
-        <Section5 />
-        <Section6 />
-      </main>
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path='/' element={<About />} />
+
+        </Routes>
       <Footer />
+      </BrowserRouter>
     </>
   );
 }
