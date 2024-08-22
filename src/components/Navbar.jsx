f3f3a551b5d3f033753f7f7d536f24597a2bc356
Amirhossein <amyrhsynshakry4@gmail.com>
2024-08-22 01:45:19 -0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,38 @@
+import { Link } from "react-router-dom";
+import image1 from '../img/user.png'
+
 export default function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <a href='#'>About</a>
-        </li>
+    <div className= "header">
+            <div className="header-team" >
+            <h1 className="header-h1">AI Developer Team</h1>
+            </div>
+           <nav>
+            <ul >
+                <li><Link to="/">About</Link></li>
+                <li><Link to="">Our team</Link></li>
+                <li><Link to="/"> Projects</Link></li>
+                <li><Link to="/">contacts</Link></li>
+               
+            </ul>
+            <div className="hide">
+                <button></button>
+            </div>
 
-        <li>
-          <a href='#'>Our team</a>
-        </li>
-
-        <li>
-          <a href='#'>Projects</a>
-        </li>
-
-        <li>
-          <a href='#'>Contacts</a>
-        </li>
-      </ul>
-    </nav>
+            </nav>
+            <div className="join-team">
+              <div className="namad">
+                <img src={image1} alt="prof" />
+              </div>
+                <div className="header-join">
+                    
+                    <button>Join team</button>
+                </div>
+            </div>
+          </div>
+                
+    
   );
 }
+
+
