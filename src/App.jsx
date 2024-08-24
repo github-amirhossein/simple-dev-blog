07172d0e38aface97af6f200c66e979cc09db071
Amirhossein <amyrhsynshakry4@gmail.com>
2024-08-24 14:16:55 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <BrowserRouter>
       <Navbar />
         <Routes>
-          <Route path='/' element={<About />} />
+          <Route path='/about' element={<About />} />
           <Route path='/contact' element={<Contact />} />
 
         </Routes>
