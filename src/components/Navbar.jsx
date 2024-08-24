f5519eb4da5572e3bfb183678e6b5e98e374201a
Amirhossein <amyrhsynshakry4@gmail.com>
2024-08-24 14:16:50 -0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
             </div>
            <nav>
             <ul >
-                <li><Link to="/">About</Link></li>
+                <li><Link to="/about">About</Link></li>
                 <li><Link to="">Our team</Link></li>
                 <li><Link to="/"> Projects</Link></li>
                 <li><Link to="/contact">contacts</Link></li>
