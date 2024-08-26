8c2e3ccbd754585bf7c2f1ea7d667c8479e6cb4e
Amirhossein <amyrhsynshakry4@gmail.com>
2024-08-25 22:09:19 -0700

    Create Donate.jsx

---
+++ src/components/Donate.jsx
@@ -0,0 +1,25 @@
+export default function Donate() {
+    return(
+        <>
+        <div className="w-full h-full">
+
+
+
+
+
+
+
+
+
+        </div>
+        
+        
+        
+        
+        
+        
+        
+        
+        </>
+    )
+}
